bb5dbec825598d6cb2ae4342ffce7e3670c2a70d
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-28 12:48:18 -0700

    instructions specify that this is to learn functional programming

--- yourOwnGame.js
+++ yourOwnGame.js
@@ -1,4 +1,6 @@
-//use map to iterate through all objects of the same type and set their movement descriptions
+// NOTE: it is an explicit goal of this project to get you familiar with functional programming. Functional programming is a whole new way of thinking that requires some practice and repetition to become natural. These prompts are designed to give you practice using functional programming, so go through them as they're written, even if you can think of other ways of doing them. 
+
+//use filter to grab all pieces of the same type, and then use map to iterate through them and set their movement descriptions
 //use map to iterate through all objects of the same type and set their collision descriptions
 //use map to iterate through all pieces for each side and randomly 'gift' half of each player's objects to the other player. Call this command 'The Great Equalizer'
 
