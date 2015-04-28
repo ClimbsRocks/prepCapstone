939778a2c701e222d0fc94e06d3990ff9a786508
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-28 12:21:59 -0700

    progress on makePiece

--- helperFunctions.js
+++ helperFunctions.js
@@ -27,14 +27,32 @@ var makeGameBoard = function(boardSize) {
 //we create an initial gameBoard of size 8x8. Feel free to modify as you'd prefer!
 makeGameBoard(8);
 
-var makePiece = function(initialPosition, name) {
+//here we're going to keep track of the count of all pieces added to our gameBoard. 
+var totalPieceCount = {};
+
+var makePiece = function(initialPosition, pieceType, playerBelongsTo) {
+  // make sure this piece is counted in our totalPieceCount object. 
+  if(totalPieceCount[pieceType]) {
+    totalPieceCount[pieceType]++;
+  } else {
+    totalPieceCount[pieceType] = 1;
+  }
+
+  // default player to Player1 if no player name is passed in, then defines a unique name for this gamePiece
+  playerBelongsTo = playerBelongsTo || 'Player1';
+  var pieceName = playerBelongsTo + pieceType + '#' + totalPieceCount[pieceType];
 
   var gamePiece = {
     movementDescription: 'use words to describe how this piece moves so your users can understand what their options are',
-    name: name,
+    collisionDescription: 'use words to explain what happens when this piece collides with another',
+    name: pieceName,
+    typeoOfPiece: '',
     imageURL: '',
+    playerBelongsTo: playerBelongsTo  // if you have a game with two (or more?!) players playing against each other, you'll want to specify which player this piece belongs to
 
   }
+
+  return gamePiece;
 };
 
 var addImageToGamePiece = function(imageURL, gamePiece) {
