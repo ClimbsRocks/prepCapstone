1cba106033ef135736f052b8603a0d32fbf6000e
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-28 15:05:35 -0700

    render now creates a square game board on the screen. adds lots of comments

--- helperFunctions.js
+++ helperFunctions.js
@@ -9,6 +9,7 @@ var makeGameBoard = function(boardSize) {
     for(var j = 0; j < boardSize; j++) {
       //set an initial pattern of alternating colors on each square). 
       if ( (i + j) % 2 === 0 ) {
+        // TODO: make default colors Telegraph colors :)
         var color = 'blue';
       } else {
         var color = 'green';
@@ -31,18 +32,37 @@ var makeGameBoard = function(boardSize) {
 //we create an initial gameBoard of size 8x8. Feel free to modify as you'd prefer!
 var gameBoard = makeGameBoard(8);
 
+// Call this function each time you make a change and want that change to appear on the screen. Otherwise, your "code" will know that the change happened, but the screen won't know that it's supposed to update. 
+// There are more elegant ways of doing this (right now we're wiping out the existing rendered gameBoard entirely and rendering the whole new one). We could just update individual pieces, but that would require us to build out more helper functions for the students, which would decrease their feeling of ownership over the whole project. This way also explicitly calls out that updating state and updating rendering are two separate things. This also lets the student focus on what they feel comfortable with and what the goal of the course is (writing JS logic/functional programming), and really not have to think about DOM or rendering stuff much at all. 
 var renderGameBoard = function(gameBoard) {
+  $('.gameBoard').html('');
   var boardSize = gameBoard.length;
   // we scale the gameBoard to the user's screen. First we find which is smaller, the height or width of the user's browser
   var browserSize = Math.min($(window).height(), $(window).width());
+  $('.gameBoard').width(browserSize - 200);
   // then we leave some room around the edges (200 pixels), and divide by the number of squares to find how large the squares should be to fill that space perfectly.
   var squareSize = (browserSize - 200) / boardSize;
   gameBoard.forEach(function(rowArr, rowIndex) {
-    rowArr.forEach(function(square, columnIndex) {
-      var squareHtml = '<div class=gameSquare "' + rowIndex + ' ' + columnIndex + '" >This is my square!</div>';
+    rowArr.forEach(function(squareObj, columnIndex) {
+      // Here we are creating the HTML that will be rendered to the DOM for each square. 
+      // HTML and JS play nicely together; you can just create a string with most of the characters that you need, add in some variables dynamically, and then when you render this to the DOM, it will interpret everything to be HTML elements and display them correctly. 
+      // We're creating a <div>, which is just a default html container that we can do whatever we want with (similar to an object in JS).
+      // We can then set "properties" on this html element. In this case, we're setting style properties to tell it how it should look on the screen. 
+        // Those style properties include it's size (height and width) in pixels (px). 
+        // We're setting it's background color to be the color of that squareObj. 
+      // To keep track of which square this is (necessary for figuring out which square was clicked on later), we set a data "property" on each square as well. 
+      // Inside of each div, we can put whatever text we want! Or none at all- it doesn't care. So we put in the text from the object at that position, if one exists
+      // TODO: refactor rowIndex and columnIndex to be data attributes, not classes?? i like the idea of being able to operate on all of them at the same time, but i can't think of what practical purpse that would have, particularly when we're doing everything in JS and then just wiping/re-rendering it all each time there's a change. 
+      var squareHtml = '<div class=gameSquare "' + rowIndex + ' ' + columnIndex + '" style="background-color:' + squareObj.color + '; height:' + squareSize + 'px; width:' + squareSize + 'px">This is my square!</div>';
+      console.log(squareHtml);
       $('.gameBoard').append(squareHtml);
     });
   });
+  // TODO: attach event listeners
+  // TODO: add image if it exists on a piece
+    // TODO: size image appropriately if it exists
+  // TODO: include text if it exists on a piece- display name by default
+  // TODO: comments for what this is doing to not intimidate students
 }
 renderGameBoard(gameBoard);
 
--- yourOwnGame.js
+++ yourOwnGame.js
@@ -4,4 +4,6 @@
 //use map to iterate through all objects of the same type and set their collision descriptions
 //use map to iterate through all pieces for each side and randomly 'gift' half of each player's objects to the other player. Call this command 'The Great Equalizer'
 
+// NOTE: there are two separate things going on: you making a change to the javascript code/logic behind the app, and rendering that new "state" to the DOM (what appears on the screen). In order to see any changes you make rendered to the screen, you need to call renderGameBoard() and pass in the updated gameBoard as an argument. 
+
 
