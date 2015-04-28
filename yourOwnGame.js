a729b50e65363545ba5c0e2c44a44129a68e6e71
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-28 12:22:17 -0700

    some early ideas for prompts to give the student

---
+++ yourOwnGame.js
@@ -0,0 +1,5 @@
+//use map to iterate through all objects of the same type and set their movement descriptions
+//use map to iterate through all objects of the same type and set their collision descriptions
+//use map to iterate through all pieces for each side and randomly 'gift' half of each player's objects to the other player. Call this command 'The Great Equalizer'
+
+
